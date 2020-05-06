66c80ccc9bd0fa6dd94afc8a7ad9fff87157fdf2
Weixian Christopher Tey <s10193226@connect.np.edu.sg>
2020-05-06 10:46:18 +0800

    Done Practical 3

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,13 +2,30 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer babyLaugh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        babyLaugh = MediaPlayer.create(this, R.raw.baby_laughing);
+        babyLaugh.start();
+
+
+
+        VideoView view = findViewById(R.id.videoView);
+        view.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid) );
+        view.start();
+
+
+
+
     }
 }
